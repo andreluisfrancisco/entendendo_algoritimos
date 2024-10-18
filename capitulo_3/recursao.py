90cc36ab0d3e1d825d5979a9e0d16f5dc496153d
André Luis Francisco <andreluis.francisco@gmail.com>
2024-10-17 23:07:28 -0300

    recursao

--- capitulo_3/recursao.py
+++ capitulo_3/recursao.py
@@ -5,11 +5,12 @@ def contagem_regressiva(numero):
     else:
         contagem_regressiva(numero - 1) 
 
+contagem_regressiva(3)
+
 def fatorial(numero):
     if numero == 1:
         return 1
     else:
         return numero * fatorial(numero - 1) 
 
-contagem_regressiva(3)
 print(fatorial(3))
